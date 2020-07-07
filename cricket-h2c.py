3514436dc1461d7f06745fdf6f89ac7151ffee2f
pranavishvar <67966849+pranavishvar@users.noreply.github.com>
2020-07-07 21:43:42 +0530

    Update cricket-h2c.py

--- cricket-h2c.py
+++ cricket-h2c.py
@@ -9,7 +9,6 @@ playerscore = 0
 computerscore=0
 turn=0
 while Toss=="b" or Toss =="s":
-
         while Toss== "b" :
            computer = random.randint(1, 6)
            player = input(" your turn: ")
@@ -18,26 +17,20 @@ while Toss=="b" or Toss =="s":
                print("player:    " , player)
                print("computer:  " , computer)
            else:
-
                print("Invalid input")
            if player==computer  :
               print('"player out "')
               break
-           elif player!=computer:
+           else:
                playerscore+=player
-
         if Toss == "b":
             print(f'"player score {playerscore}"')
             turn+=1
             Toss = "s"
         if turn==2:
-            #print("stage1 break")
             break
-
-
         while Toss == "s":
             if Toss=="s":
-
                 computer = random.randint(1, 6)
                 player = input(" your turn: ")
                 player = int(player)
@@ -49,21 +42,20 @@ while Toss=="b" or Toss =="s":
                 if computer == player:
                   print('"computer out "')
                   break
-                elif player != computer:
+                else:
                    computerscore += computer
         if Toss == "s":
             print(f'" computer score {computerscore}"')
             turn+=1
             Toss = "b"
         if turn==2:
-            #print("stage2 break")
             break
 if computerscore < playerscore:
     print('"Player won the match"')
 elif computerscore > playerscore:
     print('"computer won the match"')
 else :
-    print('"Match die"')
+    print('"Match Tie"')
 
 
 
