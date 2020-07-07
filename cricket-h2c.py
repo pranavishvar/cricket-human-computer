e6401cdd8dc3a5ec03e1edd63a51401ab69f3d1b
pranavishvar <67966849+pranavishvar@users.noreply.github.com>
2020-07-07 20:40:43 +0530

    Add files via upload
    Initial version of cricket game. Played between human and computer.

---
+++ cricket-h2c.py
@@ -0,0 +1,99 @@
+import random
+print("odd or even game")
+Toss=input("(b)Bat or (s)Bowl: ")
+if Toss== "b":
+    print("computer will bowl")
+else:
+    print("computer will bat")
+playerscore = 0
+computerscore=0
+turn=0
+while Toss=="b" or Toss =="s":
+
+        while Toss== "b" :
+           computer = random.randint(1, 6)
+           player = input(" your turn: ")
+           player=int(player)
+           if player>0 and player < 7:
+               print("player:    " , player)
+               print("computer:  " , computer)
+           else:
+
+               print("Invalid input")
+           if player==computer  :
+              print('"player out "')
+              break
+           elif player!=computer:
+               playerscore+=player
+
+        if Toss == "b":
+            print(f'"player score {playerscore}"')
+            turn+=1
+            Toss = "s"
+        if turn==2:
+            #print("stage1 break")
+            break
+
+
+        while Toss == "s":
+            if Toss=="s":
+
+                computer = random.randint(1, 6)
+                player = input(" your turn: ")
+                player = int(player)
+                if player>0 and player < 7:
+                    print("player:   " ,player)
+                    print("computer: " ,computer)
+                else:
+                    print("Invalid input")
+                if computer == player:
+                  print('"computer out "')
+                  break
+                elif player != computer:
+                   computerscore += computer
+        if Toss == "s":
+            print(f'" computer score {computerscore}"')
+            turn+=1
+            Toss = "b"
+        if turn==2:
+            #print("stage2 break")
+            break
+if computerscore < playerscore:
+    print('"Player won the match"')
+elif computerscore > playerscore:
+    print('"computer won the match"')
+else :
+    print('"Match die"')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
